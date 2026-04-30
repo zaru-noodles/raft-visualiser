5cc2c86d90d61f057dc3525a389e97b8fdee0c6b
Aidan Tay <silver14211@gmail.com>
2026-04-30 20:19:54 +0800

    improved rpc

--- rpc/transport.go
+++ rpc/transport.go
@@ -1,16 +1,13 @@
 package rpc
 
 import (
-	"net/rpc"
-	"net"
-	"log"
-	"time"
+	"fmt"
 
 	"github.com/zaru-noodles/raft-visualiser/raft"
 )
 
 type TransportRPC struct {
-	peers map[uint8]*rpc.Client
+	peers map[uint8]*PeerClient
 	inbox chan raft.Message
 }
 
@@ -19,66 +16,53 @@ func MakeTransport(RPCPort string, addrs []string, id uint8) TransportRPC {
 
 	go startServer(inbox, RPCPort)
 
-	// connects to peers' RPC server
-	peers := make(map[uint8]*rpc.Client)
+	// store addresses
+	peers := make(map[uint8]*PeerClient)
 	for i, addr := range addrs {
 		// skip own id
 		i := uint8(i)
 		if uint8(i) >= id {
-			peers[i+1] = dialWithRetry(addr)
+			peers[i+1] = MakePeerClient(addr)
 		} else {
-		    peers[i] = dialWithRetry(addr)
+		    peers[i] = MakePeerClient(addr)
 		}
 	}
-	log.Print("Connected!")
 
 	return TransportRPC{ peers: peers, inbox: inbox } 
 }
 
-// attempts to dial address indefinitely
-func dialWithRetry(address string) *rpc.Client {
-    for {
-        client, err := rpc.Dial("tcp", address)
-        if err == nil {
-            return client
-        }
-        log.Printf("Retrying connection to %s...", address)
-        time.Sleep(time.Second)
-    }
-}
-
-// opens port to allow RPC
-func startServer(inbox chan raft.Message, port string) {
-    service := &RaftService{inbox: inbox}
-    rpc.Register(service)
-
-    listener, err := net.Listen("tcp", ":" + port)
-    if err != nil {
-        log.Fatal("UNABLE TO START SERVER:", err)
-    }
-
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            continue
-        }
-        go rpc.ServeConn(conn)
-    }
-}
-
 func (t TransportRPC) SendRequestVote(peer uint8, req raft.RequestVote) (raft.RequestVoteReply, error) {
 	var reply raft.RequestVoteReply
-	log.Print("Sending RequestVote RPC...")
-    err := t.peers[peer].Call("RaftService.RequestVote", req, &reply)
-	log.Print("Recieved RequestVoteReply!")
+
+	// check if client exists
+	client := t.peers[peer].GetClient()
+	if client == nil {
+		return reply, fmt.Errorf("no connection to peer %d", peer)
+	}
+
+    err := client.Call("RaftService.RequestVote", req, &reply)
+	if err != nil {
+		go client.Close()
+
+	}
+
     return reply, err
 }
 
 func (t TransportRPC) SendAppendEntries(peer uint8, req raft.AppendEntries) (raft.AppendEntriesReply, error) {
 	var reply raft.AppendEntriesReply
-	log.Print("Sending AppendEntries RPC...")
-    err := t.peers[peer].Call("RaftService.AppendEntries", req, &reply)
-	log.Print("Recieved AppendEntriesReply!")
+
+	// check if client exists
+	client := t.peers[peer].GetClient()
+	if client == nil {
+		return reply, fmt.Errorf("no connection to peer %d", peer)
+	}
+
+    err := client.Call("RaftService.AppendEntries", req, &reply)
+	if err != nil {
+		go client.Close()
+		
+	}
     return reply, err
 }
 
